9d8c2962f5757da582bd6e4f50d86dbdd713d062
Gskull <Gskull47@gmail.com>
2022-04-01 20:48:21 -0700

    made DELETE button work by giving it a function in app.js

--- lab-starter/src/app.js
+++ lab-starter/src/app.js
@@ -1,17 +1,23 @@
+let clone = 0
 function cOne(){
     let div = document.createElement('div'); //makes the div
     div.id = 'copyed';                       //makes the div id
     div.innerHTML = '<p>one</p>';            //makes whats inside the div
     document.body.appendChild(div);          //NEED TO CHANGE LATER
+    clone ++
 }
 function cTwo(){
     let div = document.createElement('div'); //makes the div
     div.id = 'copyed';                       //makes the div id
     div.innerHTML = '<p>two</p>';            //makes whats inside the div
     document.body.appendChild(div);          //NEED TO CHANGE LATER
+    clone ++
 }
 
 function DELETE(){
-    var element = document.getElementById("copyed");
-    element.parentNode.removeChild(element);
+    while (clone !== 0){
+        var element = document.getElementById("copyed");
+        element.parentNode.removeChild(element); 
+        clone = clone - 1
+    }
 }
